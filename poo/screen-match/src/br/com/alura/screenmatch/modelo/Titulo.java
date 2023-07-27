0e9c374dab5d18d157ea5dc56d9e6d4872c01ea6
Guilherme Mikami <guilherme.mikami@hotmail.com>
2023-07-27 19:12:48 -0300

    selecionando elementos da api

--- poo/screen-match/src/br/com/alura/screenmatch/modelo/Titulo.java
+++ poo/screen-match/src/br/com/alura/screenmatch/modelo/Titulo.java
@@ -1,7 +1,11 @@
 package br.com.alura.screenmatch.modelo;
 
-public class Titulo {
+import com.google.gson.annotations.SerializedName;
+
+public class Titulo implements Comparable<Titulo> {
+    @SerializedName("Title")
     private String nome;
+    @SerializedName("Year")
     private int anoDeLancamento;
     private boolean incluidoNoPlano;
     private double somaDasAvaliacoes;
@@ -60,5 +64,17 @@ public class Titulo {
 
     public double pegaMedia() {
         return somaDasAvaliacoes / totalDeAvaliacoes;
+    }
+
+    @Override
+    public int compareTo(Titulo outroTitulo){
+        return  this.getNome().compareTo(outroTitulo.getNome());
+    }
+
+    @Override
+    public String toString() {
+        return  "nome ='" + nome + '\'' +
+                ", anoDeLancamento=" + anoDeLancamento +
+                '}';
     }
 }
